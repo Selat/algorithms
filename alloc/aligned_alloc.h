961dea0ca9c2367d7b9dd75728ca1ddec5255cae
Uladzislau Paulovich <selatnick@gmail.com>
2018-06-18 12:03:17 +0300

    Implement AlignedAlloc

---
+++ alloc/aligned_alloc.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <cstddef>
+#include <cstdlib>
+
+template <typename T, size_t alignment>
+class AlignedAlloc {
+public:
+  static_assert(alignment > 0);
+  static_assert(alignment % sizeof(T) == 0);
+  static_assert(alignment < 256);
+  typedef T value_type;
+
+  void deallocate(T *p, std::size_t n) {
+    uint8_t* aligned_ptr = reinterpret_cast<uint8_t*>(p);
+    size_t shift = *(aligned_ptr - 1);
+    uint8_t* ptr = aligned_ptr - shift;
+    free(ptr);
+  }
+
+  T* allocate(std::size_t n) {
+    uint8_t* ptr = static_cast<uint8_t*>(malloc(n * sizeof(T) + alignment));
+    size_t shift = alignment - (reinterpret_cast<uint64_t>(ptr) % alignment);
+    uint8_t* aligned_ptr = ptr + shift;
+    *(aligned_ptr - 1) = shift;
+    return reinterpret_cast<T*>(aligned_ptr);
+  }
+};
---
+++ alloc/aligned_alloc_test.cpp
@@ -0,0 +1,42 @@
+#include "aligned_alloc.h"
+
+#include <iostream>
+#include <vector>
+
+void TestLinearFill(int n) {
+  std::vector<int, AlignedAlloc<int, sizeof(int)>> data;
+  assert(((reinterpret_cast<uint64_t>(data.data())) % sizeof(int)) == 0);
+  for (int i = 0; i < n; ++i) {
+    data.push_back(i);
+  }
+
+  for (int i = 0; i < n; ++i) {
+    assert(data[i] == i);
+  }
+}
+
+void TestAddressAlignments(int tests_num) {
+  for (int i = 0; i < tests_num; ++i) {
+    std::vector<int, AlignedAlloc<int, 16>> data16;
+    data16.push_back(0);
+    assert(((reinterpret_cast<uint64_t>(data16.data())) % 16) == 0);
+
+    std::vector<int, AlignedAlloc<int, 32>> data32;
+    data32.push_back(0);
+    assert(((reinterpret_cast<uint64_t>(data32.data())) % 32) == 0);
+
+    std::vector<int, AlignedAlloc<int, 64>> data64;
+    data64.push_back(0);
+    assert(((reinterpret_cast<uint64_t>(data64.data())) % 64) == 0);
+
+    std::vector<int, AlignedAlloc<int, 128>> data128;
+    data128.push_back(0);
+    assert(((reinterpret_cast<uint64_t>(data128.data())) % 128) == 0);
+  }
+}
+
+int main() {
+  TestLinearFill(100000);
+  TestAddressAlignments(1000);
+  return 0;
+}
