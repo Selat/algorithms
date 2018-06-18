d9e50cef407e007dbd2a46b8f653f6dfac5013db
Uladzislau Paulovich <selatnick@gmail.com>
2018-06-18 12:24:46 +0300

    Address @ref2401 style comments

--- alloc/aligned_alloc.h
+++ alloc/aligned_alloc.h
@@ -1,9 +1,10 @@
 #pragma once
 
+#include <cassert>
 #include <cstddef>
 #include <cstdlib>
 
-template <typename T, std::size_t alignment>
+template <typename T, size_t alignment>
 class AlignedAlloc {
 public:
   static_assert(alignment > 0);
@@ -11,16 +12,20 @@ public:
   static_assert(alignment < 256);
   typedef T value_type;
 
-  void deallocate(T *p, std::size_t n) {
+  void deallocate(T *p, size_t /* not used */) {
+    assert(p != nullptr);
     uint8_t* aligned_ptr = reinterpret_cast<uint8_t*>(p);
-    std::size_t shift = *(aligned_ptr - 1);
+    size_t shift = *(aligned_ptr - 1);
     uint8_t* ptr = aligned_ptr - shift;
     free(ptr);
   }
 
-  T* allocate(std::size_t n) {
+  T* allocate(size_t n) {
     uint8_t* ptr = static_cast<uint8_t*>(malloc(n * sizeof(T) + alignment));
-    std::size_t shift = alignment - (reinterpret_cast<uint64_t>(ptr) % alignment);
+    if (ptr == nullptr) {
+      return nullptr;
+    }
+    size_t shift = alignment - (reinterpret_cast<size_t>(ptr) % alignment);
     uint8_t* aligned_ptr = ptr + shift;
     *(aligned_ptr - 1) = shift;
     return reinterpret_cast<T*>(aligned_ptr);
--- alloc/aligned_alloc_test.cpp
+++ alloc/aligned_alloc_test.cpp
@@ -7,7 +7,7 @@ void TestLinearFill(int n) {
   std::cout << "Testing linear vector fill..." << std::flush;
 
   std::vector<int, AlignedAlloc<int, sizeof(int)>> data;
-  assert(((reinterpret_cast<uint64_t>(data.data())) % sizeof(int)) == 0);
+  assert(((reinterpret_cast<size_t>(data.data())) % sizeof(int)) == 0);
   for (int i = 0; i < n; ++i) {
     data.push_back(i);
   }
@@ -22,21 +22,25 @@ void TestAddressAlignments(int tests_num) {
   std::cout << "Testing address alignements..." << std::flush;
 
   for (int i = 0; i < tests_num; ++i) {
+    std::vector<int, AlignedAlloc<int, 8>> data8;
+    data8.push_back(0);
+    assert(((reinterpret_cast<size_t>(data8.data())) % 8) == 0);
+
     std::vector<int, AlignedAlloc<int, 16>> data16;
     data16.push_back(0);
-    assert(((reinterpret_cast<uint64_t>(data16.data())) % 16) == 0);
+    assert(((reinterpret_cast<size_t>(data16.data())) % 16) == 0);
 
     std::vector<int, AlignedAlloc<int, 32>> data32;
     data32.push_back(0);
-    assert(((reinterpret_cast<uint64_t>(data32.data())) % 32) == 0);
+    assert(((reinterpret_cast<size_t>(data32.data())) % 32) == 0);
 
     std::vector<int, AlignedAlloc<int, 64>> data64;
     data64.push_back(0);
-    assert(((reinterpret_cast<uint64_t>(data64.data())) % 64) == 0);
+    assert(((reinterpret_cast<size_t>(data64.data())) % 64) == 0);
 
     std::vector<int, AlignedAlloc<int, 128>> data128;
     data128.push_back(0);
-    assert(((reinterpret_cast<uint64_t>(data128.data())) % 128) == 0);
+    assert(((reinterpret_cast<size_t>(data128.data())) % 128) == 0);
   }
 
   std::cout << "ok!" << std::endl;
