fc04816602a9b51000003749a0b9e72583e8c59e
Uladzislau Paulovich <selatnick@gmail.com>
2018-06-18 12:09:24 +0300

    More consistent usage of std::size_t type

--- alloc/aligned_alloc.h
+++ alloc/aligned_alloc.h
@@ -3,7 +3,7 @@
 #include <cstddef>
 #include <cstdlib>
 
-template <typename T, size_t alignment>
+template <typename T, std::size_t alignment>
 class AlignedAlloc {
 public:
   static_assert(alignment > 0);
@@ -13,14 +13,14 @@ public:
 
   void deallocate(T *p, std::size_t n) {
     uint8_t* aligned_ptr = reinterpret_cast<uint8_t*>(p);
-    size_t shift = *(aligned_ptr - 1);
+    std::size_t shift = *(aligned_ptr - 1);
     uint8_t* ptr = aligned_ptr - shift;
     free(ptr);
   }
 
   T* allocate(std::size_t n) {
     uint8_t* ptr = static_cast<uint8_t*>(malloc(n * sizeof(T) + alignment));
-    size_t shift = alignment - (reinterpret_cast<uint64_t>(ptr) % alignment);
+    std::size_t shift = alignment - (reinterpret_cast<uint64_t>(ptr) % alignment);
     uint8_t* aligned_ptr = ptr + shift;
     *(aligned_ptr - 1) = shift;
     return reinterpret_cast<T*>(aligned_ptr);
